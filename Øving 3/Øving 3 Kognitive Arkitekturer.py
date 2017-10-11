9c0f8874d7f62df383d4fcb7d05ea29eff9aaa0b
ubuu <henrik-olsvik@outlook.com>
2017-10-11 21:30:07 +0200

    Finished Exercise
    ....

--- Øving 3/Øving 3 Kognitive Arkitekturer.py
+++ Øving 3/Øving 3 Kognitive Arkitekturer.py
@@ -7,19 +7,19 @@
 # Delta = 1.2 gir Delta_Stable = 0.3 og Delta_Growing = 0.4. 
 # 
 # #2 Rule Evaluation: 
-# Since Distance_Small = 0.6 AND Delta_Growing = 0.4 then Action_None = 0.5
-# Since Distance_Small = 0.6 AND Delta_Stable = 0.3 then  Action_SlowDown = 0.45
-# Since Distance_Perfect = 0.1 AND Delta_Growing = 0.4 then Action_SpeedUp = 0.25
+# Since Distance_Small = 0.6 AND Delta_Growing = 0.4 then Action_None = 0.4
+# Since Distance_Small = 0.6 AND Delta_Stable = 0.3 then  Action_SlowDown = 0.3
+# Since Distance_Perfect = 0.1 AND Delta_Growing = 0.4 then Action_SpeedUp = 0.1
 #
 # #3 Aggregation of the rule outputs: 
-# Action: Action_BrakeHard = 0.0, Action_SlowDown = 0.45, Action_None = 0.5, Action_SpeedUp = 0.25, Action_FloorIt = 0.0
+# Action: Action_BrakeHard = 0.0, Action_SlowDown = 0.3, Action_None = 0.4, Action_SpeedUp = 0.1, Action_FloorIt = 0.0
 # 
 # #4 Defuzzification:
-# Action_COG = (((-4)*0.45 + (0)*0.5 + (4)*0.25)/ 1.20) = -0.93
+# Action_COG = (((-4)*0.3 + (0)*0.4 + (4)*0.1)/ 0.8) = -1
 # Action_COG gives action as None.
 #
 
-# Oppgave 2
+# Oppgave b
 
 # Global distance and delta
 distance = 0 
@@ -30,12 +30,14 @@ delta = 0
 # FOF 
 distanceBoundaries = [(1, 2.5),(1.5, 4.5),(3.5, 6.5),(5.5, 8.5),(7.50, 9)]
 deltaBoundaries = [(-5, -2.5),(-3.5, -0.5),(-1.5, 1.5),(0.5, 3.5),(2.5, 4)]
-actionBoundaries = [(-8, -5),(-7, -1),(-3, 3),(1, 7),(5, 8)]
+actionBoundaries = [(-8, -5, "ＡＣＴＩＯＮ:　「ＢＲＡＫＥ　ＨＡＲＤ」"),(-7, -1, "ＡＣＴＩＯＮ:　「ＳＬＯＷ　ＤＯＷＮ」"),(-3, 3, "ＡＣＴＩＯＮ:　「ＮＯＮＥ」"),(1, 7, "ＡＣＴＩＯＮ:　「ＳＰＥＥＤ　ＵＰ」"),(5, 8, "ＡＣＴＩＯＮ:　「ＦＬＯＯＲ　ＩＴ」")]
+aWP = [-8.25, -4, 0, 4, 8.25]
 
 # fuzzysets
 
+import time
+
 def triangle(position, x0, x1, x2, clip):
-    print("triangle", position, x0, x1, x2, clip)
     value = 0
     if(position >= x0 and position <= x1):
          value = (position - x0)/(x1-x0)
@@ -46,7 +48,6 @@ def triangle(position, x0, x1, x2, clip):
     return value
 
 def grade(position, x0, x1, clip):
-    print("grade", position, x0, x1, clip)
     value = 0
     if(position >= x1):
         value = 1
@@ -59,7 +60,6 @@ def grade(position, x0, x1, clip):
     return value
 
 def reverse_grade(position, x0, x1, clip):
-    print("reverse_grade", position, x0, x1, clip)
     value = 0
     if(position <= x0):
         value = 1
@@ -72,7 +72,6 @@ def reverse_grade(position, x0, x1, clip):
     return value
 
 def fuzzification(boundaries, index, indexLength, calcValue):
-    print(boundaries, index, indexLength)
     if(index == indexLength-1):
         return grade(calcValue, boundaries[0], boundaries[1], 1)
     elif(index == 0):
@@ -80,24 +79,48 @@ def fuzzification(boundaries, index, indexLength, calcValue):
     else:
         return triangle(calcValue, boundaries[0], ((boundaries[0] + boundaries[1])/2), boundaries[1], 1)
     
+def defuzzification(cog, set):
+    defuzzifiedValues = []
+    for i in range(0, len(set)):
+        defuzzifiedValues.append(fuzzification(set[i], i, len(set), cog))
+    return set[defuzzifiedValues.index(max(defuzzifiedValues))][2]
+
+
+
 # cog calculation
 
-def cog_calc():
-    print("Yet to implement")
+def cog_calc_action(fuzzySet):
+    #tilnermet integrasjon
+    weight = 0
+    for x in range(0, len(aWP)):
+        weight += fuzzySet[x]*aWP[x]
+    try:    
+        return ( weight / sum(fuzzySet))
+    except ZeroDivisionError: 
+        return 0
 
 # rule sets
 
 #TODO
 
-# main
+def rule_evaluation(fuzzyDistances, fuzzyDeltas):
+    fuzzyActions = []
+    print("「Ｅｖａｌｕａｔｉｎｇ　Ｒｕｌｅｓ！」")
+    fuzzyActions.append(fuzzyDistances[0]) #IF	distance	is	VerySmall	THEN	action	is	BrakeHard
+    fuzzyActions.append(min(fuzzyDistances[1], fuzzyDeltas[2])) #IF	distance	is	Small	AND	delta	is	Stable	THEN	action	is	SlowDown
+    fuzzyActions.append(min(fuzzyDistances[1], fuzzyDeltas[3])) #IF	distance	is	Small	AND	delta	is	Growing	THEN	action	is	None
+    fuzzyActions.append(min(fuzzyDistances[2], fuzzyDeltas[3])) #IF	distance	is	Perfect	AND	delta	is	Growing	THEN	action	is	SpeedUp
+    fuzzyActions.append(min(fuzzyDistances[4], 1 - (max((fuzzyDeltas[3]), (fuzzyDeltas[4]))))) #IF	distance	is	VeryBig	AND	(delta	is	NOT	Growing	OR	delta	is	NOT	GrowingFast) THEN	action	is	 FloorIt
+    return fuzzyActions
 
 def main():
     global distance, delta
     try:
-        distance = float(input("Please input a number for distance")) 
-        delta = float(input("Please input a number for delta")) 
+        distance = float(input("Please input a number for 「distance」 ")) 
+        delta = float(input("Please input a number for 「delta」 ")) 
     except ValueError:
         print("Invalid input, please re-enter!")
+        main()
     
     fuzzyDistances = []
     fuzzyDeltas = []
@@ -108,8 +131,8 @@ def main():
     for i in range(0, len(deltaBoundaries)):
         fuzzyDeltas.append(fuzzification(deltaBoundaries[i], i, len(deltaBoundaries), delta)) 
 
-    print(fuzzyDistances)
-    print(fuzzyDeltas)
-
+    print(defuzzification(cog_calc_action(rule_evaluation(fuzzyDistances, fuzzyDeltas)), actionBoundaries))
+    print("\n")
+    main()
 
 main()
